6da809e702998c3d868adff9e90c0172b2fa8607
leonardomenesesdev <164566383+leonardomenesesdev@users.noreply.github.com>
2024-09-13 15:33:08 -0300

    Add files via upload

---
+++ QuestTres/Soma.java
@@ -0,0 +1,14 @@
+package QuestTres;
+
+public class Soma {
+    public static void main(String[] args) {
+        int INDICE = 12;
+        int soma=0;
+        int k=1;
+        while(k<INDICE){
+            k++;
+            soma+=k;
+        }
+        System.out.println(soma);
+    }
+}
